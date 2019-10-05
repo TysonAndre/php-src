113c0ac8aade76b2741908aa13d2f29a3f088b63
Tyson Andre <tysonandre775@hotmail.com>
2019-10-05 12:52:43 -0400

    wip debug

--- travis/compile.sh
+++ travis/compile.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+export CFLAGS="-O3"
 ./buildconf --force
 ./configure \
---prefix="$HOME"/php-8.0.0-minsize-install \
---enable-debug \
+--prefix="$HOME"/php-8.0.0-minsize-nts-install \
 --enable-iconv \
 --enable-mbstring \
+--enable-dom \
+--enable-simplexml \
+--with-libxml \
+--enable-opcache \
 --enable-tokenizer \
 --enable-json \
 --enable-filter \
---enable-zts \
 --disable-all
 
 make -j8
