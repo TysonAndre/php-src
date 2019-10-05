ff47843b68376295497a4aac77c791b06214b296
Tyson Andre <tysonandre775@hotmail.com>
2019-10-05 12:44:56 -0400

    wip debug

--- travis/compile.sh
+++ travis/compile.sh
@@ -3,6 +3,11 @@
 ./configure \
 --prefix="$HOME"/php-8.0.0-minsize-install \
 --enable-debug \
+--enable-iconv \
+--enable-mbstring \
+--enable-tokenizer \
+--enable-json \
+--enable-filter \
 --enable-zts \
 --disable-all
 
